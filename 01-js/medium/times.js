3cf8a4c2ff934a164cc714fe84c9a61cf2c98c86
Shubham Jankar <shubhamjankar10@gmail.com>
2023-12-09 14:45:51 +0530

    times code

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,17 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let startTime = performance.now();
+
+    let sum =0;
+    for(let i=0;i<n;i++){
+        sum=sum+i;
+    }
+    let endTime = performance.now();
+    // return sum;
+    let timeInSeconds = (endTime - startTime) / 1000;
+
+    return { timeInSeconds };
 }
+
+console.log(calculateTime(100));
