1db4d2ff3f27d0dbc9a7856dfe6fdd9e0225168b
Shubham Jankar <shubhamjankar10@gmail.com>
2023-12-09 12:37:09 +0530

    Expenditure code file

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -1,5 +1,5 @@
 /*
-  Implement a function `calculateTotalSpentByCategory` which takes a list of transactions as parameter
+  Implement a function `calculatetotalSpentByCategory` which takes a list of transactions as parameter
   and return a list of objects where each object is unique category-wise and has total price spent as its value.
   transactions is an array where each
   Transaction - an object like 
@@ -13,8 +13,29 @@
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
-function calculateTotalSpentByCategory(transactions) {
-  return [];
+function calculatetotalSpentByCategory(transactions) {
+  
+  // Create an array to store dictionaries for each category
+  let categoryInfo = [];
+  
+  transactions.forEach(person => {
+    let { price, category } = person;
+  
+    // Check if the category is already in the array
+    let existingcategory = categoryInfo.find(info => info.category === category);
+  
+    if (!existingcategory) {
+      // If the category is not in the array, add a new dictionary
+      categoryInfo.push({ category, totalSpent: price });
+    } else {
+      // If the category is in the array, update the total price
+      existingcategory.totalSpent += price;
+    }
+  });
+  
+  // console.log(categoryInfo);
+  
+  return categoryInfo;
 }
 
-module.exports = calculateTotalSpentByCategory;
+module.exports = calculatetotalSpentByCategory;
